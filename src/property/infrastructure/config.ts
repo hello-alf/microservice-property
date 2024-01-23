57bf2ceceb5ec25e6ccde61fcd024150f5c7044a
Luis Alfredo <alf.182.u@gmail.com>
2024-01-22 22:30:19 -0400

    Modificando archivos

--- src/property/infrastructure/config.ts
+++ src/property/infrastructure/config.ts
@@ -4,8 +4,8 @@ export default registerAs('config', () => {
   return {
     mongo: {
       dbName: process.env.MONGO_DB || 'NUR-PROPERTIES-DB',
-      user: process.env.MONGO_INITDB_ROOT_USERNAME || '',
-      password: process.env.MONGO_INITDB_ROOT_PASSWORD || '',
+      user: process.env.MONGO_INITDB_ROOT_USERNAME || 'mongo',
+      password: process.env.MONGO_INITDB_ROOT_PASSWORD || 'secret',
       port: parseInt(process.env.MONGO_PORT, 10) || 27017,
       host: process.env.MONGO_HOST || 'localhost',
       connection: process.env.MONGO_CONNECTION || 'mongodb',
